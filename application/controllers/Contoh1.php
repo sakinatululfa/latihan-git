a225c76d49bc8c8147834a558da03fea719cd5f5
sakinatululfa <sakinatul.ulfa@gmail.com>
2023-09-25 16:19:32 +0700

    menambahkan folder pustaka booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Sakinatul Ulfa<br>
+        Nim 199221026<br>
+        Kelas 19.3A.26<br>
+        Saya tinggal di Jakarta Selatan<br>
+        Saya suka membaca au";
+    }
+}
