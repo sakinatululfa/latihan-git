ac473d1d9332ea71f6c4cc8fcba3c8c52fd3439d
sakinatululfa <sakinatul.ulfa@gmail.com>
2023-10-09 15:50:22 +0700

    menambahkan file latihan TEMPLATE WEB pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1>
+        <?php echo $judul ?>
+    </h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Sakinatul</li>
+        <li>Nama Belakang : Ulfa</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jakarta Selatan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulu Tangkis</li>
+        <li>Lompat Tali (Yeye) </li>
+    </ul>
+</section>
